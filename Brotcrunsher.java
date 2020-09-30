b9b9c6cc30553a0a3db1b083f8fbfff51a0bae4e
Thoxen90 <70209889+Thoxen90@users.noreply.github.com>
2020-09-30 17:08:25 +0200

    Update Brotcrunsher.java

--- Brotcrunsher.java
+++ Brotcrunsher.java
@@ -5,6 +5,7 @@ public class Brotcrunsher {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
+		//es wird eine Scanner Methode initialisier um Benutzer Eingaben abzufragen
 		int Anzahl_Datensaetze;
 		
 		System.out.println("Guten Tag dies ist Ihr Assistent zum anlegen von neuen Benutzern");
@@ -12,6 +13,7 @@ public class Brotcrunsher {
 		System.out.println("");
 		System.out.println("Wie viele Benutzer sollen angelegt werden?");
 		Anzahl_Datensaetze = s.nextInt();
+		// die erste Zahleneingabe legt fest wie viele Benutzer das System anlegt
 		System.out.println("");
 		
 		int Alter[] = new int[Anzahl_Datensaetze];
@@ -20,29 +22,36 @@ public class Brotcrunsher {
 		int sum=0, avg = 0;
 		String[] Schulabschluss = new String[Anzahl_Datensaetze];
 		boolean Eignung = false;
+		//es werden mehrere Arrays und Variablen angelegt mit denen ich später arbeite
 		
 		for(int x=0;x<Anzahl_Datensaetze;x++) {
-			int z = x+1;				
+			// eine for schleife um die einzelnen Einträge durchzugehen
+			int z = x+1;
+			// damit in der folgenden sys.out ausgabe die richtige Benutzer Nr. steht und ich nicht bei benutzer 0 anfange
 			System.out.println("Vorname für "+z+". Benutzer");
 			Vorname[x] = s.next();
+			// die nächste String Eingabe wird im Aktuellen Vornamen gespeichert
 			System.out.println("");
 			
 			System.out.println("Nachname für "+z+". Benutzer");
 			Name[x] = s.next();
+			// selbe für Nachname
 			System.out.println("");
 			
 			System.out.println("Alter für "+z+". Benutzer");
 			Alter[x] = s.nextInt();
+			// Zahlen eingabe für Alter
 			System.out.println("");
 			
 			System.out.println("Schulabschluss von "+z+". Benutzer");
 			Schulabschluss[x] = s.next();
+			// String Eingabe Schulabschluss
 			System.out.println("");
-			System.out.println(Vorname[x]+" "+Name[x]+" "+Alter[x]);
+			System.out.println(Vorname[x]+" "+Name[x]+" "+Alter[x]); //Ausgabe der eingegebenen werte und bestätigung der Eingabe
 			System.out.println("Eingabe erfolgreich");
 			System.out.println("");
 			
-			sum = sum+Alter[x];
+			sum = sum+Alter[x]; //aktuelles alter wird zu summe addiert
 		}		
 		System.out.println("Zusammenfassung Benuter:");
 		System.out.println("-------------------------------------------------------------------");
@@ -50,11 +59,13 @@ public class Brotcrunsher {
 		for(int z=0; z<Anzahl_Datensaetze; z++) {
 			String Eig = "Ist nicht geeignet";
 			String erw;
+			
 			if(Alter[z]>=18) {
 				erw = "-volljährig-";
 				}else {
 				erw = "-minderjährig-";
 			}
+			// eine if abfrage um zu ermitteln ob angelegte Benutzer volljährig ist oder nicht
 			switch (Schulabschluss[z]) {
 					case "MittlereReife": Eignung = false;
 					break;
@@ -74,6 +85,7 @@ public class Brotcrunsher {
 					break;
 					default: Eignung = false;					
 			}
+			// eine switch case abfrage um zu ermitteln ober der Angegebene Schulabschluss für den Job geeignet ist oder nicht
 			if(Eignung == true && erw == "-volljährig-") {
 				Eig = "Ist für den Job geeignet";
 			} else {
@@ -81,7 +93,7 @@ public class Brotcrunsher {
 			}
 			System.out.println(Vorname[z]+" "+Name[z]+ " "+Alter[z]+" "+erw+" "+Schulabschluss[z]+" "+Eig);
 		}
-				avg = sum / Anzahl_Datensaetze;		
+				avg = sum / Anzahl_Datensaetze;		// durchschnittsalter Benutzer
 		System.out.println("");
 		System.out.println("Durchschnittsalter: "+avg);
 		System.out.println("-------------------------------------------------------------------");
