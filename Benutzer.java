b73a0dea61b32840f33f8f2c90514205eab6be1e
Thoxen90 <70209889+Thoxen90@users.noreply.github.com>
2020-10-02 15:36:57 +0200

    Update Benutzer.java

--- Benutzer.java
+++ Benutzer.java
@@ -21,7 +21,7 @@ public class Benutzer {
 		System.out.println("");
 		
 		int Alter[] = new int[Anzahl_Datensaetze];
-		String[] Name = new String[Anzahl_Datensaetze];
+		String[] Nachname = new String[Anzahl_Datensaetze];
 		String[] Vorname = new String[Anzahl_Datensaetze];
 		int sum=0, avg = 0;
 		String[] Schulabschluss = new String[Anzahl_Datensaetze];
@@ -32,31 +32,31 @@ public class Benutzer {
 		for(int x=0;x<Anzahl_Datensaetze;x++) {
 			// eine for schleife um die einzelnen Einträge durchzugehen
 			
-			System.out.println("Vorname für "+(x+1)+". Benutzer");
+			System.out.println("Vorname für "+(x+1)+". Bewerber");
 			Vorname[x] = s.next();
 			// die nächste String Eingabe wird im Aktuellen Vornamen gespeichert
 			
 			System.out.println("");
 			
-			System.out.println("Nachname für "+(x+1)+". Benutzer");
-			Name[x] = s.next();
+			System.out.println("Nachname für "+(x+1)+". Bewerber");
+			Nachname[x] = s.next();
 			// selbe für Nachname
 			
 			System.out.println("");
 			
-			System.out.println("Alter für "+(x+1)+". Benutzer");
+			System.out.println("Alter für "+(x+1)+". Bewerber");
 			Alter[x] = s.nextInt();
 			// Zahlen eingabe für Alter
 			
 			s.nextLine();  //sinnfreier Input um einen absatz zwischen s.nextInt(); und s.nextLine() zu schaffen da sonst der s.nextline() Befehl einfach übersprungen wurde
 			
 			System.out.println("");
-			System.out.println("Schulabschluss von "+(x+1)+". Benutzer");
+			System.out.println("Schulabschluss von "+(x+1)+". Bewerber");
 			Schulabschluss[x] = s.nextLine();
 			// String Eingabe Schulabschluss
 			
 			System.out.println("");
-			System.out.println(Vorname[x]+" "+Name[x]); //Ausgabe der eingegebenen Werte und bestätigung der Eingabe
+			System.out.println(Vorname[x]+" "+Nachname[x]); //Ausgabe der eingegebenen Werte und bestätigung der Eingabe
 			System.out.println("Eingabe erfolgreich");
 			System.out.println("-----------------------------------------------------------------------------------------");
 			System.out.println("");
@@ -65,7 +65,7 @@ public class Benutzer {
 		}
 		s.close();
 		
-		System.out.println("Zusammenfassung Benuter:");
+		System.out.println("Zusammenfassung Bewerber:");
 		System.out.println("-----------------------------------------------------------------------------------------------");
 		
 		for(int z=0; z<Anzahl_Datensaetze; z++) {
@@ -134,20 +134,22 @@ public class Benutzer {
 				Eig[z] = "Ist nicht für den Job geeignet";
 			}
 			
-			System.out.println(Vorname[z]+" "+Name[z]+ " "+Alter[z]+" "+erw+" |"+Schulabschluss[z]+"| "+Eig[z]);
+			System.out.println(Vorname[z]+" "+Nachname[z]+ " "+Alter[z]+" "+erw+" |"+Schulabschluss[z]+"| "+Eig[z]);
 		}
 		
 		avg = sum / Anzahl_Datensaetze;		// durchschnittsalter Benutzer
+		
 		System.out.println("");
 		System.out.println("Durchschnittsalter: "+avg);
 		System.out.println("-----------------------------------------------------------------------------------------------");
+		
 		System.out.println("");
 		System.out.println("Zusammenfassung geeigneter Bewerber");
 		System.out.println("-----------------------------------------------------------------------------------------------");
 		
 		for(int q=0;q<Anzahl_Datensaetze;q++) {
 			if(Eig[q]=="Ist für den Job geeignet") {
-				System.out.println(Vorname[q]+" "+Name[q]);
+				System.out.println(Vorname[q]+" "+Nachname[q]);
 			}
 		}
 		
