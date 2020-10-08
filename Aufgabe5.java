36e9483e624a6ff8fe7bba532975382433617b19
Thoxen90 <70209889+Thoxen90@users.noreply.github.com>
2020-10-08 12:12:28 +0200

    Update Aufgabe5.java

--- Aufgabe5.java
+++ Aufgabe5.java
@@ -10,20 +10,22 @@ public class Aufgabe5 {
 		Scanner s = new Scanner(System.in);
 		
 		while(Monatszahl<1||Monatszahl>12) {
-		System.out.println("Bitte geben Sie eine Zahl für den Monat ein");
+		System.out.println("Bitte geben Sie eine Zahl fÃ¼r den Monat ein");
 		Monatszahl = s.nextInt();
 		if(Monatszahl<1||Monatszahl>12) {
-			System.out.println(Monatszahl+" ist keine gültige Monatszahl. Bitte geben Sie eine Zahl von 1-12 ein");
+			System.out.println(Monatszahl+" ist keine gÃ¼ltige Monatszahl. Bitte geben Sie eine Zahl von 1-12 ein");
 			System.out.println("");
 		}
 		}
+		
+		s.close();
 		
 		switch(Monatszahl) {
 		case 1: Monatsname = "Januar";
 		break;
 		case 2: Monatsname = "Februar";
 		break;
-		case 3: Monatsname = "März";
+		case 3: Monatsname = "MÃ¤rz";
 		break;
 		case 4: Monatsname = "April";
 		break;
@@ -46,7 +48,7 @@ public class Aufgabe5 {
 		}
 		
 		System.out.println("----------------------------------------------------------------------");
-		System.out.println("Die eingegebene Zahl "+Monatszahl+" steht für den Monat "+Monatsname);
+		System.out.println("Die eingegebene Zahl "+Monatszahl+" steht fÃ¼r den Monat "+Monatsname);
 	}
 
 }
