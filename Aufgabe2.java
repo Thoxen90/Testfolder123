03b499dd6c849f0b4316ac4f6bd17d17eeebb1dd
Thoxen90 <70209889+Thoxen90@users.noreply.github.com>
2020-11-04 17:09:25 +0100

    Update Aufgabe2.java

--- Aufgabe2.java
+++ Aufgabe2.java
@@ -11,6 +11,7 @@ public class Aufgabe2 {
 		double Gewicht;
 		double Größe;
 		int Kalorien = 0;
+		String Kategorie = "";
 		
 		System.out.println("Bitte geben Sie Ihre größe in Metern an:");
 		Größe = s.nextDouble();
@@ -25,22 +26,27 @@ public class Aufgabe2 {
 		
 		if(BMI < 18.5) {
 			Kalorien = 3000;
+			Kategorie = "Untergewicht";
 		}else
 		if(BMI>=18.5 && BMI <25) {
 			Kalorien = 2000;
+			Kategorie = "Normalgewicht";
 		}else
 		if(BMI>=25 && BMI <30) {
 			Kalorien = 1800;
+			Kategorie = "leichtes Übergewicht";
 		}else
 		if(BMI>=30 && BMI <40) {
 			Kalorien = 1200;
+			Kategorie = "starkes Übergewicht";
 		}else
 		if(BMI>=40) {
 			Kalorien = 700;
+			Kategorie = "extremes Übergewicht";
 		}
 		
 		System.out.println("-------------------------------------------------------------------");
-		System.out.println("Ergebnis: BMI = "+String.format("%1.1f", BMI)+"    Empfohlene Kalorien = "+Kalorien);
+		System.out.println("Ergebnis: BMI = "+String.format("%1.1f", BMI)+"  ("+Kategorie+")    Empfohlene Kalorien = "+Kalorien);
 		
 	}
 
